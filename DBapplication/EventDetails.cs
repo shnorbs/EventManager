2e442b7d518a45e1f5d9e67acb4a58633664e232
shnorbs <orbedpower@gmail.com>
2024-12-27 21:14:14 +0200

    reports and feedback

--- DBapplication/EventDetails.cs
+++ DBapplication/EventDetails.cs
@@ -13,15 +13,24 @@ namespace DBapplication
     public partial class PublicEventDetails : Form
     {
         Controller cont = new Controller();
-        public PublicEventDetails(string EventName, string StartTime, string EndTime, string VenueName, int EventID, int UserID)
+
+        static DateTime currentDay = DateTime.Now;
+        string correctFormat = currentDay.ToString("yyyy-MM-dd");
+        
+        int UserID;
+        int EventID;
+        public PublicEventDetails(string EventName, string StartTime, string EndTime, string VenueName, int eID, int uID)
         {
             InitializeComponent();
             eventName.Text = EventName;
             startTimeLbl.Text = StartTime;
             endTimeLbl.Text = EndTime;
             venueName.Text = VenueName;
-            description.Text = cont.GetEventDescription(EventID);
-            eventType.Text = "(Event Type: " + cont.GetEventType(EventID) + ")";
+            description.Text = cont.GetEventDescription(eID);
+            eventType.Text = "(Event Type: " + cont.GetEventType(eID) + ")";
+            UserID = uID;
+            EventID = eID;
+         
         }
 
         private void EventDetails_Load(object sender, EventArgs e)
@@ -31,7 +40,19 @@ namespace DBapplication
 
         private void reminderBtn_Click(object sender, EventArgs e)
         {
+            int created = cont.AddReminder(correctFormat, EventID, UserID);
+        }
 
+        private void feedbackBtn_Click(object sender, EventArgs e)
+        {
+            Feedback feedback = new Feedback(UserID, EventID);
+            feedback.Show();
+        }
+
+        private void reportBtn_Click(object sender, EventArgs e)
+        {
+            Report f = new Report(UserID, EventID);
+            f.Show();
         }
     }
 }
